dd3c41fbe5cd05a0162e47ce644ae54b47a53a54
Minh Triet Pham <>
2023-10-09 21:59:21 +0700

    done

---
+++ CollectionView0910/CollectionView0910/Data.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  CollectionView0910
+//
+//  Created by Minh Triet Pham on 09/10/2023.
+//
+
+import Foundation
+struct Data {
+    var image:String = ""
+    var name:String = ""
+    
+    init(image: String, name: String){
+        self.image = image
+        self.name = name
+    }
+}
---
+++ CollectionView0910/CollectionView0910/GridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GridCollectionViewCell.swift
+//  CollectionView0910
+//
+//  Created by Minh Triet Pham on 09/10/2023.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var NameLabel: UILabel!
+}
